2c6efc48fcdce17b8a2361110b6c52230601d08e
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-22 21:50:34 +0100

    Stful to Stless. And yaay, it still works

--- lib/main.dart
+++ lib/main.dart
@@ -24,22 +24,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
   Widget build(BuildContext context) {
     final counter = Provider.of<Counter>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
