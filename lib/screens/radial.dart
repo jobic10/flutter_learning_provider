c3cddbcd67af0b85c20c021a25518abf30369fd3
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-25 14:22:02 +0100

    Learning Radial Progress

---
+++ lib/screens/radial.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RadialProgress extends StatefulWidget {
+  const RadialProgress({Key? key}) : super(key: key);
+
+  @override
+  _RadialProgressState createState() => _RadialProgressState();
+}
+
+class _RadialProgressState extends State<RadialProgress> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
