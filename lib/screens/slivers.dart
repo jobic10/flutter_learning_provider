82d66873aa928635f31dfc5eef7d865a0000c20f
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-30 13:18:23 +0100

    Wrapped Sliver With Scaffold

--- lib/screens/slivers.dart
+++ lib/screens/slivers.dart
@@ -5,16 +5,47 @@ class SliverPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          expandedHeight: 120,
-          flexibleSpace: FlexibleSpaceBar(
-            title: Text('Hello Slivers'),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            expandedHeight: 120,
+            title: Text('Outside flexible space'),
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Hello Slivers'),
+              collapseMode: CollapseMode.pin,
+            ),
+            leading: Icon(Icons.home),
+            actions: [
+              Icon(Icons.add_location_alt),
+              Icon(Icons.add_location_alt),
+              Icon(Icons.add_location_alt),
+              Icon(Icons.add_location_alt),
+            ],
           ),
-        ),
-        // SliverList(delegate: delegate),
-      ],
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              mainAxisExtent: 200,
+              childAspectRatio: 8,
+              crossAxisSpacing: 2,
+              mainAxisSpacing: 2,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.red[100 * (index % 9)],
+                  child: Text('Grid Item : $index'),
+                );
+              },
+              childCount: 100,
+            ),
+          ),
+          // SliverList(delegate: delegate),
+        ],
+      ),
     );
   }
 }
