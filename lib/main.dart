a8ae8c204d140d1cbd18eb07b920925e66409e3c
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-09-12 02:28:44 +0100

    New model

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:learning_provider/screens/radial.dart';
-import 'package:learning_provider/screens/slivers.dart';
-import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ChangeNotifierProvider<ValueNotifier<int>>(
-        create: (context) => ValueNotifier<int>(0),
-        child: MyHomePage(
-          title: 'Flutter Demo Home Page',
-        ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => RadialProgress(),
-                ),
-              );
-            },
-            icon: Icon(Icons.battery_std_sharp),
-          ),
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => SliverPage(),
-                ),
-              );
-            },
-            icon: Icon(Icons.access_alarm_rounded),
-          ),
-        ],
-      ),
-      body: Center(
-        child: Container(
-          color: Colors.red,
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(title: Text('My App')),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(20),
+              color: Colors.green[200],
+              child: ElevatedButton(
+                child: Text('Do something'),
+                onPressed: () {},
               ),
-              Text(
-                '${2 * 7}',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
-          ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(35),
+              color: Colors.blue[200],
+              child: Text('Show something'),
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => null,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
---
+++ lib/models/my_model.dart
@@ -0,0 +1,7 @@
+class MyModel {
+  String someValue = 'Hello';
+  void doSomething() {
+    someValue = 'Goodbye';
+    print(someValue);
+  }
+}
