4ba62badf9a8d9fdcf49c5ba9b0ec9582ed95669
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-09-12 02:25:25 +0100

    Back to skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,6 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final counter = Provider.of<ValueNotifier<int>>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -61,23 +60,25 @@ class MyHomePage extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Consumer<ValueNotifier<int>>(
-              builder: (context, counter, child) => Text(
-                '${counter.value}',
+        child: Container(
+          color: Colors.red,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '${2 * 7}',
                 style: Theme.of(context).textTheme.headline4,
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => counter.value++,
+        onPressed: () => null,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
--- lib/screens/slivers.dart
+++ lib/screens/slivers.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/diagnostics.dart';
+import 'package:flutter/src/rendering/sliver_persistent_header.dart';
 
 class SliverPage extends StatelessWidget {
   const SliverPage({Key? key}) : super(key: key);
@@ -6,7 +8,7 @@ class SliverPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
+      /*  body: CustomScrollView(
         slivers: [
           SliverAppBar(
             pinned: true,
@@ -27,6 +29,19 @@ class SliverPage extends StatelessWidget {
               Icon(Icons.add_location_alt),
             ],
           ),
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 8,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+            ),
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return Container(
+                  color: Colors.blue[100 * (index % 9)],
+                  child: Text('Grid ::: $index'));
+            }),
+          ),
+
           SliverGrid(
             gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
               maxCrossAxisExtent: 100,
@@ -56,16 +71,65 @@ class SliverPage extends StatelessWidget {
             }, childCount: 10),
           ),
           SliverFixedExtentList(
-              delegate: SliverChildListDelegate([
+            delegate: SliverChildListDelegate(
+              [
                 Container(color: Colors.green, child: Text('1')),
                 Container(color: Colors.brown, child: Text('2')),
                 Container(color: Colors.indigo, child: Text('3')),
                 Container(color: Colors.blue, child: Text('4')),
-              ]),
-              itemExtent: 20)
+              ],
+            ),
+            itemExtent: 20,
+          ),
           // SliverList(delegate: delegate),
         ],
+    );*/
+
+      body: CustomScrollView(
+        slivers: [SliverPersistentHeader(delegate: MyDelegate())],
       ),
     );
   }
+}
+
+class MyDelegate implements SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement maxExtent
+  double get maxExtent => throw UnimplementedError();
+
+  @override
+  // TODO: implement minExtent
+  double get minExtent => throw UnimplementedError();
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    // TODO: implement shouldRebuild
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement showOnScreenConfiguration
+  PersistentHeaderShowOnScreenConfiguration? get showOnScreenConfiguration =>
+      throw UnimplementedError();
+
+  @override
+  // TODO: implement snapConfiguration
+  FloatingHeaderSnapConfiguration? get snapConfiguration =>
+      throw UnimplementedError();
+
+  @override
+  // TODO: implement stretchConfiguration
+  OverScrollHeaderStretchConfiguration? get stretchConfiguration =>
+      throw UnimplementedError();
+
+  @override
+  // TODO: implement vsync
+  TickerProvider? get vsync => throw UnimplementedError();
 }
