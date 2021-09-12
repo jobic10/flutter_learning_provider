f4650c849c1052588cb6a137318a9b327cfc1ff2
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-09-12 02:36:39 +0100

    Provider has been provided

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:learning_provider/models/my_model.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('My App')),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(20),
-              color: Colors.green[200],
-              child: ElevatedButton(
-                child: Text('Do something'),
-                onPressed: () {},
+    return Provider<MyModel>(
+      create: (BuildContext context) => MyModel(),
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: Text('My App')),
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Consumer<MyModel>(
+                builder: (BuildContext context, MyModel model, child) =>
+                    Container(
+                  padding: const EdgeInsets.all(20),
+                  color: Colors.green[200],
+                  child: ElevatedButton(
+                    child: Text('Do something'),
+                    onPressed: () {
+                      model.doSomething();
+                    },
+                  ),
+                ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(35),
-              color: Colors.blue[200],
-              child: Text('Show something'),
-            ),
-          ],
+              Consumer<MyModel>(
+                builder: (BuildContext context, MyModel model, child) =>
+                    Container(
+                  padding: const EdgeInsets.all(35),
+                  color: Colors.blue[200],
+                  child: Text(model.someValue),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
